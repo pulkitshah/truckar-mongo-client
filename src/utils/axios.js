2d0382a45155802696fe4566e1b64fe887b1e62d
Pulkit Shah <pulkit.works@gmail.com>
2023-11-04 12:49:50 +0530

    Correct Auth Path in API folder

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create();
-axiosInstance.defaults.baseURL = "http://localhost:4000";
+// axiosInstance.defaults.baseURL = "http://localhost:4000";
 
 axiosInstance.interceptors.response.use(
   (response) => response,
