e9f3f5b622cb0ba167d41e2555fcb97581eacfb4
Pulkit Shah <pulkit.works@gmail.com>
2023-11-04 11:28:09 +0530

    Added cors in middleware

--- src/middleware.js
+++ src/middleware.js
@@ -19,6 +19,17 @@ export default async function handler(req, res, next) {
         : process.env.jwtSecret_PROD.toString()
     );
     req.user = decoded.user;
+
+    res.headers.append("Access-Control-Allow-Credentials", "true");
+    res.headers.append("Access-Control-Allow-Origin", "*"); // replace this your actual origin
+    res.headers.append(
+      "Access-Control-Allow-Methods",
+      "GET,DELETE,PATCH,POST,PUT"
+    );
+    res.headers.append(
+      "Access-Control-Allow-Headers",
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
     next();
   } catch (error) {
     res.status(401).json({ errors: [{ msg: "Token is not valid" }] });
